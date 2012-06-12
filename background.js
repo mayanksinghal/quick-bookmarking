73adc2398630770a3e88c7f73c2dceaee86ef0ba
Mayank Singhal <manku.singhal@gmail.com>
2012-06-12 08:17:12 +0530

    Some improvements

--- background.js
+++ background.js
@@ -4,7 +4,11 @@
 
 var qbm = function() {
 
-   var bookmarks = {};
+   var bookmarks = "";
+   var waiting = false;
+   var currentText = "";
+   var suggestFunctionDelayed;
+   var defaultSuggestion = "";
 
    var suggestion = function(content, description) {
       if (content === "") {
@@ -20,7 +24,16 @@ var qbm = function() {
    }
 
    var setDefault = function(suggestion) {
+      defaultSuggestion = suggestion;
       chrome.omnibox.setDefaultSuggestion(suggestion);
+   }
+
+   var treeLoader = function(bookmarkTree) {
+      bookmarks = bookmarkTree[0];
+      if (waiting) {
+         waiting = false;
+         setSuggestions();
+      }
    }
 
    var reset = function() {
@@ -29,17 +42,68 @@ var qbm = function() {
 
    // When user starts to input, load bookmarks.
    var startHandler = function() {
-      // TODO Load Bookmarks to bookmarks var
+      chrome.bookmarks.getTree(treeLoader);
    }
 
    // When the user accepts an option.
    var acceptHandler = function(text) {
-      console.log("Accepted [" + text);
+      console.log("Accepted [" + text + "]");
       // TODO Handle command
    }
 
-   var getSuggestions = function(text) {
-      return [];
+
+   var commandType = function(command, startsWith) {
+      return command.toLowerCase().indexOf(startsWith.toLowerCase()) === 0;
+   }
+
+   var getAction = function(command) {
+      if (commandType(command, "A")) {
+         return {con: "A ", desc: "Add"};
+      } else if (commandType(command, "L")) {
+         return {con: "L ", desc: "Load"};
+      }
+      return {con: "A ", desc: "Add?"};
+   }
+
+   var getPath = function(command) {
+      if (command.length <= 2) {
+         return "";
+      } else {
+         return command.substring(2);
+      }
+   }
+
+   var getSuggestedPaths = function(path) {
+      var paths = [];
+      console.log("Path [" + path + "]");
+      for (var i = 0; i < bookmarks.children.length; i++) {
+         paths.push({con: bookmarks.children[i].title,
+            desc: bookmarks.children[i].title});
+         console.log("SuggestedPath [" + bookmarks.children[i].title + "]");
+      }
+      return paths;
+   }
+
+   var setSuggestions = function() {
+      if (bookmarks === "") {
+         console.log("Waiting");
+         waiting = true;
+         return;
+      }
+      var suggestions = [];
+
+      var action = getAction(currentText);
+      var path = getPath(currentText);
+
+      var suggestedPaths = getSuggestedPaths(path);
+
+      for (var i = 0; i < suggestedPaths.length; i++) {
+         suggestions.push(suggestion(action.con + suggestedPaths[i].con,
+                  action.desc + " " + suggestedPaths[i].desc));
+      }
+
+      // TODO Populate Suggestions.
+      suggestFunctionDelayed(suggestions);
    }
 
    return {
@@ -50,9 +114,12 @@ var qbm = function() {
             acceptHandler(text);
          });
          chrome.omnibox.onInputChanged.addListener(function(text, suggestFn) {
-            suggestFn(getSuggestions(text));
+            console.log(text);
+            suggestFunctionDelayed = suggestFn;
+            currentText = text;
+            setSuggestions();
          });
-      }
+     }
    }
 } ();
 qbm.init();
